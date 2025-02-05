c082dd312f1fe7bf50d9f672e097e029b7642b1b
durwangpatole8@gmail.com <durwangpatole8@gmail.com>
2025-02-05 13:56:38 +0530

    Project Complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Home.css";
-import Navbar from "../../components/navbar/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
 import play_icon from '../../assets/play_icon.png';
 import info_icon from '../../assets/info_icon.png';
-import TitleCards from "../../components/titleCards/TitleCards";
-import Footer from "../../components/footer/Footer";
+import TitleCards from "../../components/TitleCards/TitleCards";
+import Footer from "../../components/Footer/Footer";
 const Home = () => {
   return (
     <div className="home">
